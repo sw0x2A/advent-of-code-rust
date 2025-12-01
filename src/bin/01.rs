5fa5e2a9ce74310bb4e8911781c8970bc5393543
sw0x2A <sw@00101010.org>
2025-12-01 18:18:03 +0100

    Day 1: Secret Entrance

---
+++ src/bin/01.rs
@@ -0,0 +1,101 @@
+#![allow(unexpected_cfgs)]
+#![warn(clippy::all, clippy::pedantic)]
+
+use adv_code_2025::start_day;
+use anyhow::{Ok, Result, bail};
+use code_timing_macros::time_snippet;
+use const_format::concatcp;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+const DAY: &str = "01";
+const INPUT_FILE: &str = concatcp!("input/", DAY, ".txt");
+
+const TEST: &str = "\
+L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82
+"; // TODO: Add the test input
+
+fn main() -> Result<()> {
+    start_day(DAY);
+
+    //region Part 1
+    println!("=== Part 1 ===");
+
+    #[allow(clippy::items_after_statements)]
+    fn part1<R: BufRead>(reader: R) -> Result<usize> {
+        let mut dial = 50;
+        let mut count = 0;
+        for line in reader.lines().map_while(Result::ok) {
+            let (dir, dist) = line.split_at(1);
+            let dist: i32 = dist.parse().unwrap();
+            match dir {
+                "L" => dial = (dial + 100 - dist) % 100,
+                "R" => dial = (dial + dist) % 100,
+                _ => bail!("Invalid direction"),
+            }
+            if dial == 0 {
+                count += 1;
+            }
+        }
+        Ok(count)
+    }
+
+    // TODO: Set the expected answer for the test input
+    assert_eq!(3, part1(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part1(input_file)?);
+    println!("Result = {result}");
+    //endregion
+
+    //region Part 2
+    println!("\n=== Part 2 ===");
+
+    #[allow(clippy::items_after_statements)]
+    fn part2<R: BufRead>(reader: R) -> Result<usize> {
+        let mut dial = 50;
+        let mut count = 0;
+        for line in reader.lines().map_while(Result::ok) {
+            let (dir, dist) = line.split_at(1);
+            let dist: i32 = dist.parse().unwrap();
+            match dir {
+                "L" => {
+                    for _ in 0..dist {
+                        dial = (dial + 99) % 100;
+                        if dial == 0 {
+                            count += 1;
+                        }
+                    }
+                }
+                "R" => {
+                    for _ in 0..dist {
+                        dial = (dial + 1) % 100;
+                        if dial == 0 {
+                            count += 1;
+                        }
+                    }
+                }
+                _ => bail!("Invalid direction"),
+            }
+        }
+        Ok(count)
+    }
+
+    assert_eq!(6, part2(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part2(input_file)?);
+    println!("Result = {result}");
+    //endregion
+
+    Ok(())
+}
--- src/bin/NN.rs
+++ src/bin/NN.rs
@@ -1,9 +1,12 @@
-use anyhow::*;
-use std::fs::File;
-use std::io::{BufRead, BufReader};
+#![allow(unexpected_cfgs)]
+#![warn(clippy::all, clippy::pedantic)]
+
+use adv_code_2025::start_day;
+use anyhow::{Ok, Result};
 use code_timing_macros::time_snippet;
 use const_format::concatcp;
-use adv_code_2025::*;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 const DAY: &str = "NN"; // TODO: Fill the day
 const INPUT_FILE: &str = concatcp!("input/", DAY, ".txt");
@@ -18,9 +21,10 @@ fn main() -> Result<()> {
     //region Part 1
     println!("=== Part 1 ===");
 
+    #[allow(clippy::items_after_statements)]
     fn part1<R: BufRead>(reader: R) -> Result<usize> {
         // TODO: Solve Part 1 of the puzzle
-        let answer = reader.lines().flatten().count();
+        let answer = reader.lines().map_while(Result::ok).count();
         Ok(answer)
     }
 
@@ -29,12 +33,13 @@ fn main() -> Result<()> {
 
     let input_file = BufReader::new(File::open(INPUT_FILE)?);
     let result = time_snippet!(part1(input_file)?);
-    println!("Result = {}", result);
+    println!("Result = {result}");
     //endregion
 
     //region Part 2
     // println!("\n=== Part 2 ===");
     //
+    // #[allow(clippy::items_after_statements)]
     // fn part2<R: BufRead>(reader: R) -> Result<usize> {
     //     Ok(0)
     // }
@@ -43,7 +48,7 @@ fn main() -> Result<()> {
     //
     // let input_file = BufReader::new(File::open(INPUT_FILE)?);
     // let result = time_snippet!(part2(input_file)?);
-    // println!("Result = {}", result);
+    // println!("Result = {result}");
     //endregion
 
     Ok(())
